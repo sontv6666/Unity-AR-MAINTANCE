074952888168b9c4d259996fb8763d53846e4901
Tinh Nguyen <tinhps28445@fpt.edu.vn>
2025-03-06 14:58:28 +0700

    scale profile and allcourse 6 3 25

--- Assets/Code/Scale.cs
+++ Assets/Code/Scale.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class Scale : MonoBehaviour
 {
-    public GameObject loginScreen;
+    public RectTransform loginScreen;
 
     public RectTransform scrollViewRect;
+
+    public RectTransform scrollViewProfile;
     public float phoneHeight = 1920f; // Chiều cao thiết kế cho điện thoại
     public float tabletHeight = 1024f; // Chiều cao thiết kế cho tablet
+
+    private Vector2 lastScreenSize;
 
     // Start is called before the first frame update
     void Start()
     {
         print("Screen width: " + Screen.width);
         print("Screen height: " + Screen.height);
+        lastScreenSize = new Vector2(Screen.width, Screen.height);
         if (loginScreen != null)
         {
             LoginFormResize();
@@ -27,6 +32,11 @@ public class Scale : MonoBehaviour
             AdjustScrollViewSize();
         }
 
+        if (scrollViewProfile != null)
+        {
+            AdjustScrollViewProfile();
+        }
+
 
     }
 
@@ -34,6 +44,25 @@ public class Scale : MonoBehaviour
     void Update()
     {
 
+        if (Screen.width != lastScreenSize.x || Screen.height != lastScreenSize.y)
+        {
+            lastScreenSize = new Vector2(Screen.width, Screen.height);
+
+            if (loginScreen != null)
+            {
+                LoginFormResize();
+            }
+
+            if (scrollViewRect != null)
+            {
+                AdjustScrollViewSize();
+            }
+
+            if (scrollViewProfile != null)
+            {
+                AdjustScrollViewProfile();
+            }
+        }
 
     }
 
@@ -44,9 +73,10 @@ public class Scale : MonoBehaviour
 
         if (aspectRatio >= 0.7f && aspectRatio <= 0.8f) // Thêm điều kiện kiểm tra
         {
-            Vector3 position = loginScreen.transform.position;
-            position.y += 625;
-            loginScreen.transform.position = position;
+            loginScreen.anchoredPosition = new Vector2(
+                loginScreen.anchoredPosition.x,
+                400
+            );
         }
 
     }
@@ -73,4 +103,31 @@ public class Scale : MonoBehaviour
 
         }
     }
+
+
+    void AdjustScrollViewProfile()
+    {
+        float screenHeight = Screen.height;
+        float aspectRatio = (float)Screen.width / Screen.height;
+
+        // Nếu tỉ lệ màn hình gần với tablet (ví dụ: 4:3)
+        if (aspectRatio >= 0.7f && aspectRatio <= 0.8f)
+        {
+            float scaleFactor = tabletHeight / phoneHeight;
+            scrollViewProfile.sizeDelta = new Vector2(
+                scrollViewProfile.sizeDelta.x,
+                scrollViewProfile.sizeDelta.y * scaleFactor
+            );
+
+
+            scrollViewProfile.anchoredPosition = new Vector2(
+                scrollViewProfile.anchoredPosition.x,
+                0
+            );
+
+
+
+        }
+    }
+
 }
