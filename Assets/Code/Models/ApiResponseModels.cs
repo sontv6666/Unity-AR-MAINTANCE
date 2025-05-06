64da126aa6aab4b4a6b6a30622b8d7817d51ef93
sontv6666 <sontv6666@gmail.com>
2025-05-06 14:34:54 +0700

    save

--- Assets/Code/Models/ApiResponseModels.cs
+++ Assets/Code/Models/ApiResponseModels.cs
@@ -66,7 +66,7 @@ namespace Models
         public int? numberOfLessons;
         public int? numberOfParticipants;
         public Instruction[] instructions;
-        
+        public int? numberOfStaffScan;
         public string machineTypeId;
        
     }
